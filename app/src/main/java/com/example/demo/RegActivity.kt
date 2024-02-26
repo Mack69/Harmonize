89a83cf5b14d578a81197d6527eb14fd2040e5fd
mack69 <ghoshmayukh.janai@gmail.com>
2024-02-26 19:59:41 +0530

    Authentication testing

--- app/src/main/java/com/example/demo/RegActivity.kt
+++ app/src/main/java/com/example/demo/RegActivity.kt
@@ -3,15 +3,17 @@ package com.example.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.widget.Toast
 import com.example.demo.databinding.ActivityMainBinding
 import com.example.demo.databinding.ActivityRegBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-
+import java.util.regex.Pattern
 
 class RegActivity : AppCompatActivity() {
     private lateinit var dbref: CollectionReference
@@ -22,25 +24,43 @@ class RegActivity : AppCompatActivity() {
 
         binding = ActivityRegBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        dbref= FirebaseFirestore.getInstance().collection("User")
+//        dbref= FirebaseFirestore.getInstance().collection("User")
         binding.Signupbtn.setOnClickListener{
 
             val confirmPass =binding.editTextTextPassword2.text.toString()
+
             val user=User(
                 name=binding.editTextText.text.toString(),
                 email=binding.editTextTextEmailAddress.text.toString(),
                 phone=binding.editTextPhone.text.toString(),
                 password=binding.editTextTextPassword.text.toString())
+            val phnlen= user.phone.length
 
+            if(user.name.isNotEmpty()&& user.email.isNotEmpty() && user.phone.isNotEmpty() && user.password.isNotEmpty())
+            {
 
-            if(user.name.isNotEmpty()&& user.email.isNotEmpty() && user.phone.isNotEmpty() && user.password.isNotEmpty()){
-
-                if (user.password == confirmPass) {
-                    registerUser(user)
+                for (char in user.name){
+                    if (char.isDigit()==true){
+                        binding.editTextText.setError("Invalid Username")
+                        return@setOnClickListener
+                    }
                 }
-                else{
-                    Toast.makeText(this, "Password is not matching" ,Toast.LENGTH_SHORT).show()
+
+                if(!Pattern.matches(Patterns.EMAIL_ADDRESS.pattern(),user.email)){
+                    binding.editTextTextEmailAddress.setError("Invalid Email Address")
+                    return@setOnClickListener
                 }
+
+                if(phnlen!=10){
+                    binding.editTextPhone.setError("Invalid Phone Number")
+                    return@setOnClickListener
+                }
+
+                if (user.password != confirmPass) {
+                    binding.editTextTextPassword2.setError("Password is not Matching")
+                    return@setOnClickListener
+                }
+                registerUser(user)
 
             }
             else{
@@ -49,17 +69,23 @@ class RegActivity : AppCompatActivity() {
             }
         }
     }
-
     private fun registerUser(user:User){
-
-        dbref.add(user)
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(user.email,user.password)
             .addOnCompleteListener{
                 Log.d("Successful Reg", "registerUser: ")
                 Toast.makeText(this, "Registered Successfully" ,Toast.LENGTH_SHORT).show()
-
-
+                clearInputFields()
             }.addOnFailureListener{
                 Log.d("Unsuccessful Reg", "registerUser: ")
+                Toast.makeText(this, "Register Failed" ,Toast.LENGTH_SHORT).show()
             }
     }
+    fun clearInputFields() {
+        binding.editTextText.text.clear()
+        binding.editTextTextEmailAddress.text.clear()
+        binding.editTextPhone.text.clear()
+        binding.editTextTextPassword.text.clear()
+        binding.editTextTextPassword2.text.clear()
+    }
+
 }
